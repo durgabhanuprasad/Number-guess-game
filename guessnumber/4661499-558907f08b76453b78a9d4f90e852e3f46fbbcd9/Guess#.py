26ac93b81d5c4a662c2d804eac1cbd2703b66d15
durgabhanuprasad <bhanuprasadm3138@gmail.com>
2025-05-05 21:47:35 +0530

    Update Guess#.py

--- guessnumber/4661499-558907f08b76453b78a9d4f90e852e3f46fbbcd9/Guess#.py
+++ guessnumber/4661499-558907f08b76453b78a9d4f90e852e3f46fbbcd9/Guess#.py
@@ -1,24 +1,24 @@
 import random #bring in the random number
 import time
-number=random.randint(1, 200) #pick the number between 1 and 200
+number=random.randint(1, 200) 
 
 def intro():
     print("May I ask you for your name?")
-    name=input() #asks for the name
+    name=input() 
     print(name + ", we are going to play a game. I am thinking of a number between 1 and 200")
     time.sleep(.5)
     print("Go ahead. Guess!")
 
 def pick():
     guessesTaken = 0
-    while guessesTaken < 6: #if the number of guesses is less than 6
+    while guessesTaken < 6: 
         time.sleep(.25)
-        enter=input("Guess: ") #inserts the place to enter guess
-        try: #check if a number was entered
-            guess = int(enter) #stores the guess as an integer instead of a string    
+        enter=input("Guess: ") 
+        try: 
+            guess = int(enter)  
 
-            if guess<=200 and guess>=1: #if they are in range
-                guessesTaken=guessesTaken+1 #adds one guess each time the player is wrong
+            if guess<=200 and guess>=1: 
+                guessesTaken=guessesTake=1
                 if guessesTaken<6:
                     if guess<number:
                         print("The guess of the number that you have entered is too low")
@@ -29,12 +29,12 @@ def pick():
                         print("Try Again!")
                 if guess==number:
                     break #if the guess is right, then we are going to jump out of the while block
-            if guess>200 or guess<1: #if they aren't in the range
+            if guess>200 or guess<1: 
                 print("Silly Goose! That number isn't in the range!")
                 time.sleep(.25)
                 print("Please enter a number between 1 and 200")
 
-        except: #if a number wasn't entered
+        except: 
             print("I don't think that "+enter+" is a number. Sorry")
             
     if guess == number:
